eeed7a09ff8f721c2bcd3122fc765eff80bfcafe
ruoyu <hunger@jirengu.com>
2018-04-19 18:14:30 +0800

    笔记本列表使用 elementui

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import baseURLConfig from './config-baseURL'
-
-console.log(baseURLConfig)
+import { Message } from 'element-ui'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.baseURL = baseURLConfig.baseURL
@@ -25,11 +24,11 @@ export default function request(url, type = 'GET', data = {}) {
       if(res.status === 200) {
         resolve(res.data)
       }else {
-        console.error(res.data)
+        Message.error(res.data.msg)
         reject(res.data)
       }
     }).catch(err=>{
-      console.error({msg: '网络异常'})
+      Message.error(res.data.msg)
       reject({msg: '网络异常'})
     })
   })
